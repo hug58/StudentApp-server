d0f4d5284d11b3bb1ff42164e2b5a37409eef3a2
hug58 <hugomontaez@gmail.com>
2021-07-22 18:59:55 -0400

    deploy

--- app_database/database.py
+++ app_database/database.py
@@ -14,15 +14,20 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 
+if config("DEBUG") == "1": 
 
-POSTGRES_USER = config("POSTGRES_USER", cast=str)
-POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
-POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
-POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
-POSTGRES_DB = config("POSTGRES_DB", cast=str)
+    POSTGRES_USER = config("POSTGRES_USER", cast=str)
+    POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
+    POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
+    POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
+    POSTGRES_DB = config("POSTGRES_DB", cast=str)
 
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", default= f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
+    SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", default= f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
 
+else:
+    SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
+
+        
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL 
