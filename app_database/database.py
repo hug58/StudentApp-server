9aa5eac6a271156f047675538eeb25ff0330d7bc
hug58 <hugomontaez@gmail.com>
2021-07-22 17:00:04 -0400

    deploy postgres and docker

--- app_database/database.py
+++ app_database/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
 """
@@ -12,6 +13,7 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 
+
 POSTGRES_USER = config("POSTGRES_USER", cast=str)
 POSTGRES_PASSWORD = config("POSTGRES_PASSWORD", cast=Secret)
 POSTGRES_SERVER = config("POSTGRES_SERVER", cast=str, default="db")
@@ -19,8 +21,8 @@ POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
 POSTGRES_DB = config("POSTGRES_DB", cast=str)
 
 
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
-
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL",f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}")
+#SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", default=)
 
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
